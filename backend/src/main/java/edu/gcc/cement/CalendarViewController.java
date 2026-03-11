36a6d63a96ca85060b388111a64068f778643ba4
ellaK48 <160775833+ellaK48@users.noreply.github.com>
2026-03-10 22:39:39 -0400

    Added free blocks and remove button to calendar. Fixed major bugs with the calendar

--- backend/src/main/java/edu/gcc/cement/CalendarViewController.java
+++ backend/src/main/java/edu/gcc/cement/CalendarViewController.java
@@ -15,11 +15,20 @@ public class CalendarViewController {
 
         //added these for the example course vvv
 
-        Time time1 = new Time("T", 9, 10);
-        Time time2 = new Time("R", 9, 10);
-        ArrayList<Time> times = new ArrayList<>();
-        times.add(time1);
-        times.add(time2);
+        Time time1 = new Time("T", 9*60, 10*60); //*60 to convert to minutes
+        Time time2 = new Time("R", 9*60, 10*60);
+        ArrayList<Time> times1 = new ArrayList<>();
+        times1.add(time1);
+        times1.add(time2);
+
+        Time time3 = new Time("T", 11*60, 12*60);
+        Time time4 = new Time("R", 8*60, 9*60);
+        Time time5 = new Time("F", 9*60, 10*60);
+
+        ArrayList<Time> times2 = new ArrayList<>();
+        times2.add(time3);
+        times2.add(time4);
+        times2.add(time5);
 
         ArrayList<String> professors = new ArrayList<String>();
         professors.add("Graybill, Keith B.");
@@ -39,11 +48,13 @@ public class CalendarViewController {
 //            }
 //        });
 
-        //example course to send to the calendar since we don't have a database
+        //example courses to send to the calendar since we don't have a database
 
-        Course course = new Course("PRINCIPLES OF ACCOUNTING I", "ACCT 201", "A", "ACCT", professors, times, "2023_Fall", "SHAL 316", 3, "");
+        Course course1 = new Course("PRINCIPLES OF ACCOUNTING I", "ACCT 201", "A", "ACCT", professors, times1, "2023_Fall", "SHAL 316", 3, "");
+        Course course2 = new Course("Comp Sci Class", "COMP 240", "B", "COMP", professors, times2, "2023_Fall", "STEM 100", 3, "");
         ArrayList<Course> courses = new ArrayList<>();
-        courses.add(course);
+        courses.add(course1);
+        courses.add(course2);
 
         app.get("/api/schedule", ctx -> {
             try {
@@ -54,6 +65,21 @@ public class CalendarViewController {
             }
         });
 
+        app.post("/api/remove-course", ctx -> {
+
+            Map<String, String> data = ctx.bodyAsClass(Map.class);
+
+            String name = data.get("name");
+            String section = data.get("section");
+
+            courses.removeIf(c ->
+                    c.getName().equals(name) &&
+                            c.getSection().equals(section)
+            );
+
+            ctx.result("Course removed");
+
+        });
 
 
     }
