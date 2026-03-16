8c38c866a2ec76cfea2f700fcf5e1e5207068b0e
timheiser <heisertd23@gcc.edu>
2026-03-16 00:14:24 -0400

    added filters to frontend

--- backend/src/main/java/edu/gcc/cement/SearchController.java
+++ backend/src/main/java/edu/gcc/cement/SearchController.java
@@ -2,8 +2,7 @@ package edu.gcc.cement;
 
 import io.javalin.Javalin;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class SearchController {
 
@@ -19,12 +18,69 @@ public class SearchController {
 
             String query = ctx.queryParam("q");
 
-            Search newSearch = new Search(query, new ArrayList<>(), new ArrayList<>());
+            Search newSearch = new Search(query, new ArrayList<>());
 
-            List<Course> results = newSearch.getResults();
+            ArrayList<Course> results = newSearch.getResults();
 
             ctx.json(results);
 
+        });
+
+        app.get("/api/filters", ctx -> {
+
+            Search search = new Search("", new ArrayList<>());
+
+            ArrayList<Course> courses = search.getResults();
+
+            HashSet<String> departments = new HashSet<>();
+            HashSet<String> professors = new HashSet<>();
+            HashSet<Integer> credits = new HashSet<>();
+            HashSet<String> days = new HashSet<>();
+
+            for (Course c : courses) {
+
+                departments.add(c.getDepartment());
+                credits.add(c.getCredits());
+
+                for (String prof : c.getProfessors()) {
+                    professors.add(prof);
+                }
+
+                for (Time t : c.getTimes()) {
+                    days.add(t.getDay());
+                }
+            }
+
+            Map<String,Object> data = new HashMap<>();
+
+            data.put("departments", departments);
+            data.put("professors", professors);
+            data.put("credits", credits);
+            data.put("days", days);
+
+            ArrayList<Filter> filters = new ArrayList<>();
+
+            String dept = ctx.queryParam("dept");
+            if (dept != null && !dept.isBlank()) {
+                filters.add(new Filter(dept, Type.DEPT));
+            }
+
+            String prof = ctx.queryParam("prof");
+            if (prof != null && !prof.isBlank()) {
+                filters.add(new Filter(prof, Type.PROF));
+            }
+
+            String creds = ctx.queryParam("creds");
+            if (creds != null && !prof.isBlank()) {
+                filters.add(new Filter(creds, Type.CREDITS));
+            }
+
+            String day = ctx.queryParam("days");
+            if (day != null && !day.isBlank()) {
+                filters.add(new Filter(day, Type.DAYS));
+            }
+
+            ctx.json(data);
         });
     }
 }
