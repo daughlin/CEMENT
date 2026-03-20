9e84c5e41d5a3bf1f12518eda68071f582993620
daughlin <cole@mattandroxanne.com>
2026-03-20 01:25:14 -0400

    cleaned up main and moved start logic to run

--- backend/src/main/java/edu/gcc/cement/Main.java
+++ backend/src/main/java/edu/gcc/cement/Main.java
@@ -14,50 +14,14 @@ import io.javalin.http.staticfiles.Location;
 
 public class Main {
     public static void main(String[] args) {
+        run();
 
-        // example code for how to load a saved schedule
-//        Schedule schedule = ScheduleStorage.loadSchedule();
-        //System.out.println("Current Schedule:");
-//        System.out.println("--------------------");
-//        for (Course course : schedule.getCourses()) {
-//            System.out.println(course.getName());
-//            System.out.println(course.getCourseCode());
-//            System.out.println(course.getProfessors().getFirst());
-//            System.out.println("--------------------");
-//        }
+    }
 
-            // example code for how to use a search with filters
-            // use "n credit(s)" format to search by credits
-//            ArrayList<Filter> filters = new ArrayList<Filter>();
-//            filters.add(new Filter("3", Type.CREDITS));
-//            filters.add(new Filter("reli", Type.DEPT));
-//            filters.add(new Filter("ansberry", Type.PROF));
-//            filters.add(new Filter("9:00", Type.START));
-//            filters.add(new Filter("12:00", Type.END));
-//            filters.add(new Filter("MWF", Type.DAYS));
-//            try {
-//                Search testSearch = new Search("", filters);
-//                System.out.println("--------------------");
-//                System.out.println(testSearch.getResults().size());
-//                for (Course course : testSearch.getResults()) {
-//                    System.out.println(course.getName());
-//                    System.out.println(course.getCourseCode());
-//                    System.out.println(course.getProfessors().getFirst());
-//                    System.out.println(course.getCredits() + " credit(s)");
-//                    //System.out.println(course.getDays());
-//                    System.out.println("" + course.getDays() + ": " + course.getNiceTime());
-//                    //System.out.println();
-//                    System.out.println("--------------------");
-//
-////                     adding everything in search results to saved schedule as example
-////                     call ScheduleStorage.saveSchedule(schedule) every time something is added or removed from the schedule
-//                    //schedule.addCourse(course);
-//                    //ScheduleStorage.saveSchedule(schedule);
-//                }
-//            } catch (Exception e) {
-//                System.out.println(e.getMessage());
-//            }
-
+    /**
+     * Function to run the program
+     */
+    public static void run(){
         Javalin app = Javalin.create(config -> {
             config.staticFiles.add(staticFiles -> {
                 staticFiles.directory = "frontend";
@@ -67,12 +31,5 @@ public class Main {
 
         CalendarViewController.registerRoutes(app);
         SearchController.registerRoutes(app);
-    }
-
-    /**
-     * Function to run the program
-     */
-    public static void run(){
-
     }
 }
